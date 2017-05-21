11afe912abe55f0bace66cb597eb88c0c160f8b9
Kodeir <kodeir@gmail.com>
2017-05-21 17:00:20 +0300

    more file checks

--- src/com/kodeir/jcore/files/FileMaster.java
+++ src/com/kodeir/jcore/files/FileMaster.java
@@ -12,6 +12,8 @@ public class FileMaster {
         System.out.println("File separator is: \"" + File.separator + "\"");
         System.out.println("File pathSeparator is: \"" + File.pathSeparator + "\"");
         System.out.println("FileSystems separator is: \"" + FileSystems.getDefault().getSeparator() + "\"");
+
+        System.out.println("Check if /testdata/testfile.txt is reachable with separator: " + new File("."+File.separator+"testdata"+File.separator+"testfile.txt").exists());
     }
 
 }
