5f197fb20f6aa77897d5da8128c510c4c9d41f8e
kodeir <kodeir@gmail.com>
2017-07-05 17:31:02 +0300

    algoTest improvements

--- src/test/java/com/kodeir/jcore/AlgoTest.java
+++ src/test/java/com/kodeir/jcore/AlgoTest.java
@@ -1,6 +1,5 @@
 package com.kodeir.jcore;
 
-import com.kodeir.jcore.Algo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,69 +15,35 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class AlgoTest {
 
-    private int[] expected;
-    private int[] result;
+    private int[] array = {0};
+    private int[] mirrorArray = {0};
+    int sum = 0;
 
-    public AlgoTest(int[] expected, int[] result) {
-        this.expected = expected;
-        this.result = result;
+    public AlgoTest(int[] expected, int[] result, int[] sumArray) {
+        this.array = expected;
+        this.mirrorArray = result;
+        sum = sumArray[0];
     }
 
     @Parameterized.Parameters
     public static Collection createAlgos() {
-        return Arrays.asList(new Integer[][][] {
-                {{0},{0}},
-                {{0,1},{1,0}},
-                {{0,1,2},{2,1,0}},
-                {{0,1,2,3,4},{4,3,2,1,0}},
-                {{0,1,2,3,4,5,6,7,8,9},{9,8,7,6,5,4,3,2,1,0}}
+        return Arrays.asList(new int[][][] {
+                {{0},{0},{0}},
+                {{0,1},{1,0},{1}},
+                {{0,1,2},{2,1,0},{3}},
+                {{0,1,2,3,4},{4,3,2,1,0},{10}},
+                {{0,1,2,3,4,5,6,7,8,9},{9,8,7,6,5,4,3,2,1,0},{45}}
         });
     }
 
     @Test
     public void revertArrayTest() throws Exception {
-        assertArrayEquals(expected, Algo.revertArray(result));
+        assertArrayEquals(array, Algo.revertArray(mirrorArray));
     }
 
     @Test
-    public void revertArray1() throws Exception {
-        int[] a = {0};
-        int[] b = {0};
-        assertArrayEquals(a, Algo.revertArray(b));
-    }
-
-    @Test
-    public void revertArray2() throws Exception {
-        int[] a = {0,1};
-        int[] b = {1,0};
-        assertArrayEquals(a, Algo.revertArray(b));
-    }
-
-    @Test
-    public void revertArray3() throws Exception {
-        int[] a = {0,1,2};
-        int[] b = {2,1,0};
-        assertArrayEquals(a, Algo.revertArray(b));
-    }
-
-    @Test
-    public void revertArray5() throws Exception {
-        int[] a = {0,1,2,3,4};
-        int[] b = {4,3,2,1,0};
-        assertArrayEquals(a, Algo.revertArray(b));
-    }
-
-    @Test
-    public void revertArray10() throws Exception {
-        int[] a = {0,1,2,3,4,5,6,7,8,9};
-        int[] b = {9,8,7,6,5,4,3,2,1,0};
-        assertArrayEquals(a, Algo.revertArray(b));
-    }
-
-    @Test
-    public void sumArray0() throws Exception {
-        int[] a = {1,2,3};
-        assertEquals(6,Algo.sumArray(a),0);
+    public void sumArrayTest() throws Exception {
+        assertEquals(sum,Algo.sumArray(array),0);
     }
 
 }
