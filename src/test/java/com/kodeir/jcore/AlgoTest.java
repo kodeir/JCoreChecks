fbd5616b25cc14bcab139ef93b50d1a9372db5fb
kodeir <kodeir@gmail.com>
2017-07-05 17:17:14 +0300

    maven added

--- src/test/java/com/kodeir/jcore/AlgoTest.java
+++ src/test/java/com/kodeir/jcore/AlgoTest.java
@@ -1,6 +1,8 @@
 package com.kodeir.jcore;
 
+import com.kodeir.jcore.Algo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
@@ -11,7 +13,32 @@ import static org.junit.Assert.*;
 /**
  * Created by Sergei "Kodeir" Riabinin on 15/03/17.
  */
+@RunWith(Parameterized.class)
 public class AlgoTest {
+
+    private int[] expected;
+    private int[] result;
+
+    public AlgoTest(int[] expected, int[] result) {
+        this.expected = expected;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Collection createAlgos() {
+        return Arrays.asList(new Integer[][][] {
+                {{0},{0}},
+                {{0,1},{1,0}},
+                {{0,1,2},{2,1,0}},
+                {{0,1,2,3,4},{4,3,2,1,0}},
+                {{0,1,2,3,4,5,6,7,8,9},{9,8,7,6,5,4,3,2,1,0}}
+        });
+    }
+
+    @Test
+    public void revertArrayTest() throws Exception {
+        assertArrayEquals(expected, Algo.revertArray(result));
+    }
 
     @Test
     public void revertArray1() throws Exception {
